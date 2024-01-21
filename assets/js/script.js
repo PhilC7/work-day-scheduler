535169f9d7baf3551224ca83f41343cd77b6af56
Phil Cooper <philjcooper@outlook.com>
2024-01-21 16:38:12 +0000

    added local storage functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,14 +32,41 @@ hour.forEach(element => {
 
     // create a time slot for each hour.
     var timeSlot = $(`
-        <div class="row time-block">
-            <div class="col-2 hour">
-                ${time}
-            </div>
-            <textarea class="col-9 ${checkedTime}"></textarea>
+        <div data-timeslot class="row time-block">
+            <div class="col-2 hour">${time}</div>
+            <textarea name="text" class="col-9 ${checkedTime}"></textarea>
             <button type="submit" class="col-1 saveBtn"><i class="fa-solid fa-floppy-disk"></i></button>
         </div>`)
     container.append(timeSlot); // append time slot to container div.
 });
 
+// event listener to save input
+var save = $(".saveBtn");
+save.on("click", saveToLocalStorage) // save inputs when button is clicked 
 
+
+// Function to save data to local storage
+function saveToLocalStorage() {
+    var textInput = $('textarea'); // set variable for textarea
+
+    // for each textarea, create a key: value pairing
+    textInput.each(function (index) {
+        var key = "hour-" + hour[index];
+        var value = $(this).val();
+        localStorage.setItem(key, value); //set local storage
+    });
+}
+
+// Function to load data from local storage
+function loadFromLocalStorage() {
+    var textInput = $('textarea');// set variable for textarea
+
+    // for each textarea to get any existing data
+    textInput.each(function (index) {
+        var key = "hour-" + hour[index];
+        var value = localStorage.getItem(key); // get data and set the value.
+        $(this).val(value);
+    });
+}
+// call function to pre-load any existing data from local storage.
+loadFromLocalStorage();
