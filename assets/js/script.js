ddc2f3fdf25ebcf8c728f4314bb173cd2706e502
Phil Cooper <philjcooper@outlook.com>
2024-01-21 12:22:03 +0000

    loop added to create timeslot for each hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,26 +3,37 @@
 ******************************/
 var currentDay = $("#currentDay");
 var container = $(".container");
+var currentHour = dayjs().format("h A"); // variable to display hour - i.e. 9 AM
 
 
 // Set variable with current date
-var now = dayjs().format("dddd, DD  MMMM YYYY");
-currentDay.text(now); // Set text as current date
+var todaysDate = dayjs().format("dddd, DD  MMMM YYYY");
+currentDay.text(todaysDate); // Set text as current date
 
-console.log(dayjs().hour());
 
 // function to create a time slot for each hour between 9am and 5pm.
 function createTimeSlot() {
-    var timeBLock = $(`
-    <div class="row time-block">
-        <div class="col-2 hour">
-            9 AM
-        </div>
-        <div class="col-9"></div>
-        <button type="submit" class="col-1 saveBtn"><i class="fa-solid fa-floppy-disk"></i></button>
-    </div>`)
 
-    container.append(timeBLock);
+    // for loop to create time slot block for each hour.
+    for (let hour = 9; hour <= 17; hour++) {
+        var time = dayjs(`${hour}`).format("h A");
+        var timeBLock = $(`
+        <div class="row time-block">
+            <div class="col-2 hour">
+                ${time}
+            </div>
+            <textarea class="col-9"></textarea>
+            <button type="submit" class="col-1 saveBtn"><i class="fa-solid fa-floppy-disk"></i></button>
+        </div>`)
+        container.append(timeBLock);
+    }
 }
 
+
 createTimeSlot();
+
+// save button test
+var save = $(".saveBtn");
+save.on("click", function () {
+    console.log("Saved");
+})
