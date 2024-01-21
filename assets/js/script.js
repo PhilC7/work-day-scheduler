82d2d9a0f7ffc689f3ecf27cbcfa70abe83f58a1
Phil Cooper <philjcooper@outlook.com>
2024-01-21 10:25:18 +0000

    added tester time block to see if it works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,28 @@
+/******************************
+ *   Global Variables.  
+******************************/
 var currentDay = $("#currentDay");
+var container = $(".container");
 
+
+// Set variable with current date
 var now = dayjs().format("dddd, DD  MMMM YYYY");
-currentDay.text(now);
+currentDay.text(now); // Set text as current date
+
+console.log(dayjs().hour());
+
+// function to create a time slot for each hour between 9am and 5pm.
+function createTimeSlot() {
+    var timeBLock = $(`
+    <div class="row time-block">
+        <div class="col-2 hour">
+            9 AM
+        </div>
+        <div class="col-9"></div>
+        <button type="submit" class="col-1 saveBtn"><i class="fa-solid fa-floppy-disk"></i></button>
+    </div>`)
+
+    container.append(timeBLock);
+}
+
+createTimeSlot();
